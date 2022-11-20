c13296fe0e1939f6834c8a171e3a0c0ba83f134c
hussainmoatsim <64757004+hussainmoatsim@users.noreply.github.com>
2022-11-20 20:07:01 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import Home from "./pages/home";
+import logo from './logo.svg';
+import React, { Component }  from 'react';
+import './App.css';
+
+function App() {
+  return <Home />
+}
+
+export default App;
