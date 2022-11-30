7d1c69bd0d89adbcb78e2c2408996cc6cfa1c298
Abdul Muiz <113927276+TorqueKill@users.noreply.github.com>
2022-11-30 08:43:06 +0500

    hm

--- frontend/src/API/api.js
+++ frontend/src/API/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-let url = `http://localhost:3000/api`
+let url = `http://localhost:3001/api`
 
 // for all functions the resulting data can be accessed by using .data on the returned object
 
