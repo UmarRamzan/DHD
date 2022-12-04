f2f196c49779544fb8fad06a5a1039f3b0808b3b
UmarRamzan <mumarramzanapp@gmail.com>
2022-12-05 00:38:20 +0500

    More Settings

--- frontend/src/pages/DoctorSettings.js
+++ frontend/src/pages/DoctorSettings.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { accountGetInfo, doctorGetInfo, removeAccount, removeDoctor } from "../API/api";
 import { UserState } from "../UserState";
+import sha1 from 'sha1';
 
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
@@ -19,9 +20,12 @@ const DoctorSettings = () => {
     const [userData, setUserData] = useState({})
     const [accountData, setAccountData] = useState({})
     const [editing, setEditing] = useState(false)
+    const [changingPassword, setChangingPassword] = useState(true)
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [oldPassword, setOldPassword] = useState('')
+    const [newPassword, setNewPassword] = useState('')
     const [accountType, setAccountType] = useState('patient')
     const [error, setError] = useState('')
 
@@ -37,12 +41,20 @@ const DoctorSettings = () => {
 
     }, [])
 
-    const handleEdit = () => {
-        setEditing(true)
+    useEffect(() => {
+        setError('')
+    }, [oldPassword, newPassword])
+
+    const handleEditConfirm = () => {
+        setEditing(false)
     }
 
     const handleChangePassword = () => {
-
+        if (sha1(oldPassword) != accountData.password) {
+            setError("Old password is incorrect")
+        } else {
+            setEditing(false)
+        }
     }
 
     const handleDelete = async () => {
@@ -64,15 +76,15 @@ const DoctorSettings = () => {
             
             <Tabs defaultActiveKey="account" className="mb-3" variant="tabs" style={{margin:"15px 100px"}}>
                 <Tab eventKey="account" title="Account">
-                    {!editing &&
+                    {!editing && !changingPassword &&
                     <Card style={{ width: '650px', margin:"0px 100px", textAlign: "left" }}>
                         <Card.Body>
                             <Card.Title>Account Information</Card.Title>
                             <Card.Text>{"Email: " + accountData.email}</Card.Text>
                             <Card.Text>{"Account Type: " + accountData.accountType}</Card.Text>
                             
-                            <Button variant="outline-secondary" onClick={handleEdit} style={{margin:"5px"}}>Edit</Button>
-                            <Button variant="outline-secondary" onClick={handleChangePassword} style={{margin:"5px"}}>Change Password</Button>
+                            <Button variant="outline-secondary" onClick={()=>{setEditing(true)}} style={{margin:"5px"}}>Edit</Button>
+                            <Button variant="outline-secondary" onClick={()=>{setChangingPassword(true)}} style={{margin:"5px"}}>Change Password</Button>
                             <Button variant="outline-danger" onClick={handleDelete} style={{margin:"5px"}}>Delete Account</Button>
                         </Card.Body>
                     </Card>
@@ -89,15 +101,35 @@ const DoctorSettings = () => {
                                 </Form.Group>
 
                                 <Button onClick={()=>{setEditing(false)}} variant="outline-secondary" type="submit" style={{width: "300px"}}>Cancel</Button>
-                                <Button variant="outline-success" type="submit" style={{width: "300px"}}>Confirm</Button>
+                                <Button onClick={handleEditConfirm}variant="outline-success" type="submit" style={{width: "300px"}}>Confirm</Button>
 
                                 {error && <Alert variant='danger'>{error}</Alert>}
 
                                 </Stack>   
                             </Card.Body>
                         </Card>
-                        
                     }
+
+                    {changingPassword &&
+                    <Card style={{ width: '650px', margin:"0px 100px", textAlign: "left" }}>
+                            <Card.Body>
+                                <Card.Title>Change Password</Card.Title>
+                                <Stack gap={1} className="col-12 mx-auto">
+                            
+                                <Form.Group className="mb-3">
+                                    <Form.Control style={{ width: '300px', margin: '10px 0px'}} type="password" placeholder="Old Password" value={oldPassword} onChange={(e)=>{setOldPassword(e.target.value)}} required/>
+                                    <Form.Control style={{ width: '300px', margin: '10px 0px'}} type="password" placeholder="New Password" value={newPassword} onChange={(e)=>{setNewPassword(e.target.value)}} required/>
+                                </Form.Group>
+
+                                <Button onClick={()=>{setChangingPassword(false)}} variant="outline-secondary" type="submit" style={{width: "300px"}}>Cancel</Button>
+                                <Button onClick={(handleChangePassword)}variant="outline-success" type="submit" style={{width: "300px"}}>Confirm</Button>
+
+                                {error && <Alert variant='danger'>{error}</Alert>}
+
+                                </Stack>   
+                            </Card.Body>
+                    </Card>
+                    }   
                 </Tab>
                 <Tab eventKey="personal" title="Personal">
                     {!editing && userData && 
@@ -109,7 +141,7 @@ const DoctorSettings = () => {
                             {userData.dateOfBirth && <Card.Text>{"Date of Birth: " + userData.dateOfBirth.substring(0,10)}</Card.Text>}
                             <Card.Text>{"Gender: " + userData.gender}</Card.Text>
                             
-                            <Button variant="outline-success" onClick={handleEdit}>Edit</Button>
+                            <Button variant="outline-success" onClick={()=>{setEditing(true)}}>Edit</Button>
                         </Card.Body>
                     </Card>
                     }
